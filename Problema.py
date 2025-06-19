19641d44cf83ed13e7ade9826b8eb1a4112b95e5
Esteban <aballayesteban@gmail.com>
2025-06-19 10:06:33 -0300

    commit inicial

---
+++ Problema.py
@@ -0,0 +1,8 @@
+class Producto:
+    def init(self, nombre, precio):
+        self.nombre = nombre
+        self.precio = precio
+    
+    def  calcularIVA(precio): 
+        return precio * 1.19
+    
